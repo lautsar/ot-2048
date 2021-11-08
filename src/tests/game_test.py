e8e36d868369276d29862ac66b74c1e1a207a37d
Sari Lautanala <sari.lautanala@helsinki.fi>
2021-11-08 21:55:50 +0200

    Testi tyhjien ruutujen tunnistamiselle

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -4,9 +4,13 @@ import game
 class TestGame(unittest.TestCase):
     def setUp(self):
         self.new_game = game.Game(4)
-        print(self.new_game.map)
         self.new_game.map = [[0, 2, 2, 0], [4, 0, 2, 16], [4, 128, 16, 16], [2, 0, 2, 0]]
-        print(self.new_game.map)
+    
+    def test_empty_tiles_regocnized(self):
+        blanks = [(0, 0), (1, 1), (3, 1), (0, 3), (3, 3)]
+        new_list = self.new_game.empty_tiles()
+
+        self.assertListEqual(new_list, blanks)
 
     def test_move_left_works(self):
         self.new_game.move_left()
@@ -21,4 +25,9 @@ class TestGame(unittest.TestCase):
     def test_move_up_works(self):
         self.new_game.move_up()
         moved = [[8, 2, 4, 32], [2, 128, 16, 0], [0, 0, 2, 0], [0, 0, 0, 0]]
+        self.assertListEqual(self.new_game.map, moved)
+
+    def test_move_down_works(self):
+        self.new_game.move_down()
+        moved = [[0, 0, 0, 0], [0, 0, 4, 0], [8, 2, 16, 0], [2, 128, 2, 32]]
         self.assertListEqual(self.new_game.map, moved)
